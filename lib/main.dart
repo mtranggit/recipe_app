14a303dadc000e37097eb243f34ebfd38dbe7073
Michael Trang <michael.trang@hotmail.com>
2021-05-29 23:20:57 +1000

    Build recipe detail page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'model/recipe.dart';
+import 'recipe_detail.dart';
 
 void main() {
   runApp(RecipeApp());
@@ -57,8 +58,21 @@ class _MyHomePageState extends State<MyHomePage> {
           child: ListView.builder(
         itemCount: Recipe.samples.length,
         itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return RecipeDetail(recipe: Recipe.samples[index]);
+                  },
+                ),
+              );
+            },
+            child: buildRecipeCard(Recipe.samples[index]),
+          );
           //   return Text(Recipe.samples[index].label);
-          return buildRecipeCard(Recipe.samples[index]);
+          // return buildRecipeCard(Recipe.samples[index]);
         },
       )),
     );
@@ -66,27 +80,27 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget buildRecipeCard(Recipe recipe) {
     return Card(
-      elevation: 2.0,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            Image(image: AssetImage(recipe.imageUrl)),
-            const SizedBox(
-              height: 14.0,
-            ),
-            Text(
-              recipe.label,
-              style: const TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.w700,
-                fontFamily: 'Palatino',
-              ),   
-            ),
-          ],
-        ),
-      )
-    );
+        elevation: 2.0,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Image(image: AssetImage(recipe.imageUrl)),
+              const SizedBox(
+                height: 14.0,
+              ),
+              Text(
+                recipe.label,
+                style: const TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w700,
+                  fontFamily: 'Palatino',
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
